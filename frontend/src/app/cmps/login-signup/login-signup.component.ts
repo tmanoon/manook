e0371a491d1eb03390a9a6636e30235927ff197b
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-09 21:06:27 +0300

    Create login signup cmp

---
+++ frontend/src/app/cmps/login-signup/login-signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login-signup',
+  templateUrl: './login-signup.component.html',
+  styleUrl: './login-signup.component.scss'
+})
+export class LoginSignupComponent {
+
+}
