53ae13459092521082729ac2039159d9455dc071
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-13 14:41:30 +0300

    Add functions to util service

--- frontend/src/app/services/util.service.ts
+++ frontend/src/app/services/util.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class UtilService {
 
   constructor() { }
+
+  public loadFromStorage(entity: string): object | false {
+    const userData = localStorage.getItem(entity)
+    if (userData) return JSON.parse(userData)
+    return false
+  }
+
+  public setToStorage(entity: string, data: object): void {
+    localStorage.setItem(entity, JSON.stringify(data))
+  }
+
+  public makeId(length: number = 10): string {
+    let txt = ''
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (let i = 0; i < length; i++) {
+      txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return txt
+  }
 }
