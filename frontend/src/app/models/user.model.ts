bc97231166e3829e959f99dda841a19b9f9b92c3
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-13 15:25:59 +0300

    Add coins property to user model

--- frontend/src/app/models/user.model.ts
+++ frontend/src/app/models/user.model.ts
@@ -2,7 +2,9 @@ import { ClothingItem } from "./clothingitem.model"
 
 export interface User {
     fullName: string,
+    password: string,
     _id: string,
+    coins: number,
     wishlist: ClothingItem[],
     email: string,
     isSubscribed: boolean,
