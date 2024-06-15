b8f1639ec6f72dfff4753940098499dea47f7a51
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-15 23:42:18 +0300

    Add basic conditional logic to login signup div

--- frontend/src/app/cmps/login-signup/login-signup.component.ts
+++ frontend/src/app/cmps/login-signup/login-signup.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { userService } from '../../services/user.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'login-signup',
   templateUrl: './login-signup.component.html',
   styleUrl: './login-signup.component.scss'
 })
-export class LoginSignupComponent {
+export class LoginSignupComponent implements OnInit {
+
+  private userService = inject(userService)
+
+  loggedInUser: User | null = null
+  userClickAndState = { 
+    isClicked: false,
+    isUserLoggedIn: false
+  }
+
+  ngOnInit(): void {
+    this.userService.loggedInUser$
+      .subscribe(user => {
+        this.loggedInUser = user
+      })
+    this.userClickAndState.isUserLoggedIn = !!this.loggedInUser
+  }
+
+  onUserClick() {
+    this.userClickAndState.isClicked = !this.userClickAndState.isClicked
+  }
+
 
 }
