2a2902ba9de9749b65dac32e2e80364c225b168b
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-17 23:49:07 +0300

    Add signup routing to routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { UserSignupComponent } from './pages/user-signup/user-signup.component';
 
 
 const routes: Routes = [
   {
+    path: 'signup',
+    component: UserSignupComponent
+  },
+  {
     path: '',
     component: HomeComponent
-  }
+  },
 ];
 
 @NgModule({
