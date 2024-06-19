70b512d3778516cea4a2f5a4c0a4778adb598aef
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-20 01:15:46 +0300

    Add subscriptions to necessary observables of component

--- frontend/src/app/cmps/home-clothing-item-preview/home-clothing-item-preview.component.ts
+++ frontend/src/app/cmps/home-clothing-item-preview/home-clothing-item-preview.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ClothingItemService } from '../../services/clothingitem.service';
+import { Subject, map, take, takeUntil } from 'rxjs';
+import { ClothingItem } from '../../models/clothingitem.model';
 
 @Component({
   selector: 'home-clothing-item-preview',
   templateUrl: './home-clothing-item-preview.component.html',
   styleUrl: './home-clothing-item-preview.component.scss'
 })
-export class HomeClothingItemPreviewComponent {
+export class HomeClothingItemPreviewComponent implements OnInit, OnDestroy{
+
+  private clothingItemService = inject(ClothingItemService)
+  private destroySubject$ = new Subject()
+
+  clothes!: ClothingItem[]
+
+
+  ngOnInit(): void {
+    this.clothingItemService.clothes$
+      .pipe(
+        take(4),
+        takeUntil(this.destroySubject$)
+      )
+      .subscribe(clothes => {
+        this.clothes = clothes
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject$.next(null)
+    this.destroySubject$.complete()
+  }
+
 
 }
