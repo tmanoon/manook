e7ed52a163cae628573874d6cca843fbacf6b068
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-19 23:25:05 +0300

    Add class variables to set and change name property in filterby from appheader

--- frontend/src/app/cmps/app-header/app-header.component.ts
+++ frontend/src/app/cmps/app-header/app-header.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ClothingItemService } from '../../services/clothingitem.service';
+import { FilterBy } from '../../models/filterby.model';
+import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.scss'
 })
-export class AppHeaderComponent {
+export class AppHeaderComponent implements OnInit, OnDestroy {
 
-  discount = {isDiscount: true, sumOfDiscount: 40}
+  private clothingItemService = inject(ClothingItemService)
+  private filterSubject$ = new Subject()
+  private destroySubject$ = new Subject()
+  public discount = {isDiscount: true, sumOfDiscount: 40}
+  public filterBy!: FilterBy
+
+  ngOnInit(): void {
+    this.clothingItemService.filterBy$
+      .pipe(takeUntil(this.destroySubject$))
+      .subscribe(filterBy => {
+        this.filterBy = filterBy
+      })
+
+      this.filterSubject$
+        .pipe(
+          debounceTime(500),
+          distinctUntilChanged(),
+          takeUntil(this.destroySubject$)
+        )
+        .subscribe(() =>
+          this.clothingItemService.setFilter(this.filterBy)
+        )
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject$.next(null)
+    this.destroySubject$.complete()
+  }
+
+  onSetFilter(term: string) {
+    this.filterSubject$.next(term)
+  }
+
 }
