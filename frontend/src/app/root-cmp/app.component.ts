86e8722621b214ec2edcbaf713d4df6ac3412036
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-23 13:07:52 +0300

    Make root app component load all clothing items when initialized

--- frontend/src/app/root-cmp/app.component.ts
+++ frontend/src/app/root-cmp/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ClothingItemService } from '../services/clothingitem.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  private clothingItemService = inject(ClothingItemService)
+  
+  ngOnInit(): void {
+    this.clothingItemService.loadClothes()
+    .pipe(
+      take(1)
+    )
+    .subscribe({
+      error: err => console.log('err', err)
+    })
+  }
 }
