7ec13cf30212cfb1caca3823dcbb3e20e99c45dc
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-23 22:35:39 +0300

    Add route to clothing item index page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UserSignupComponent } from './pages/user-signup/user-signup.component';
+import { ClothingItemIndexComponent } from './pages/clothing-item-index/clothing-item-index.component';
 
 
 const routes: Routes = [
   {
     path: 'signup',
     component: UserSignupComponent
+  },
+  {
+    path: 'clothing-item',
+    component: ClothingItemIndexComponent
   },
   {
     path: '',
