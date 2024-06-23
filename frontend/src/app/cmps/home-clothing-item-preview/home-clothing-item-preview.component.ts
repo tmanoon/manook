865b7456d8b84f08a3a7a81f3fa163e8698eb798
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-23 20:13:18 +0300

    Edit logic of functionality to get four first entities in clothing item

--- frontend/src/app/cmps/home-clothing-item-preview/home-clothing-item-preview.component.ts
+++ frontend/src/app/cmps/home-clothing-item-preview/home-clothing-item-preview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { ClothingItemService } from '../../services/clothingitem.service';
-import { Subject, map, take, takeUntil } from 'rxjs';
+import { Subject, filter, map, take, takeUntil, tap } from 'rxjs';
 import { ClothingItem } from '../../models/clothingitem.model';
 
 @Component({
@@ -8,29 +8,28 @@ import { ClothingItem } from '../../models/clothingitem.model';
   templateUrl: './home-clothing-item-preview.component.html',
   styleUrl: './home-clothing-item-preview.component.scss'
 })
-export class HomeClothingItemPreviewComponent implements OnInit, OnDestroy{
+export class HomeClothingItemPreviewComponent implements OnInit, OnDestroy {
 
   private clothingItemService = inject(ClothingItemService)
   private destroySubject$ = new Subject()
 
-  clothes!: ClothingItem[]
+  clothes: ClothingItem[] = []
 
 
   ngOnInit(): void {
     this.clothingItemService.clothes$
       .pipe(
-        take(4),
-        takeUntil(this.destroySubject$)
+        takeUntil(this.destroySubject$),
+        map(clothes => clothes.slice(0, 4))
       )
       .subscribe(clothes => {
         this.clothes = clothes
       })
   }
 
+
   ngOnDestroy(): void {
     this.destroySubject$.next(null)
     this.destroySubject$.complete()
   }
-
-
 }
