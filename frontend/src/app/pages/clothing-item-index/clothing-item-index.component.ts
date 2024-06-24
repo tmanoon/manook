013e30d160464aed3ab50de3bb9adf9ec716a127
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-24 15:53:14 +0300

    Change the way of subscription to data from service in component

--- frontend/src/app/pages/clothing-item-index/clothing-item-index.component.ts
+++ frontend/src/app/pages/clothing-item-index/clothing-item-index.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ClothingItem } from '../../models/clothingitem.model';
+import { Observable, Subject, retry, take, takeUntil, tap } from 'rxjs';
+import { ClothingItemService } from '../../services/clothingitem.service';
 
 @Component({
   selector: 'clothing-item-index',
   templateUrl: './clothing-item-index.component.html',
   styleUrl: './clothing-item-index.component.scss'
 })
-export class ClothingItemIndexComponent {
+export class ClothingItemIndexComponent implements OnInit, OnDestroy {
 
+  private clothingItemService = inject(ClothingItemService)
+  private destroySubject$ = new Subject()
+  clothes$!: ClothingItem[]
+
+  ngOnInit(): void {
+    this.clothingItemService.clothes$
+    .pipe(
+      takeUntil(this.destroySubject$),
+      take(1),
+      retry(1),
+      tap(clothes => this.clothes$ = clothes)
+    )
+    .subscribe({
+      error: err => {
+        console.log('err', err)
+        throw err
+      }
+    }
+    )
+  }
+  ngOnDestroy(): void {
+    this.destroySubject$.next(null)
+    this.destroySubject$.complete()
+  }
 }
