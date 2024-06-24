961865106f08a8cc9cde35056b7c0a52e5e9fb83
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-24 15:37:44 +0300

    Make functionality and subscription simpler using async pipe

--- frontend/src/app/cmps/home-clothing-item-preview/home-clothing-item-preview.component.ts
+++ frontend/src/app/cmps/home-clothing-item-preview/home-clothing-item-preview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { ClothingItemService } from '../../services/clothingitem.service';
-import { Subject, filter, map, take, takeUntil, tap } from 'rxjs';
+import { Observable, Subject, filter, map, take, takeUntil, tap } from 'rxjs';
 import { ClothingItem } from '../../models/clothingitem.model';
 
 @Component({
@@ -8,28 +8,15 @@ import { ClothingItem } from '../../models/clothingitem.model';
   templateUrl: './home-clothing-item-preview.component.html',
   styleUrl: './home-clothing-item-preview.component.scss'
 })
-export class HomeClothingItemPreviewComponent implements OnInit, OnDestroy {
+export class HomeClothingItemPreviewComponent {
+
 
   private clothingItemService = inject(ClothingItemService)
-  private destroySubject$ = new Subject()
-
-  clothes: ClothingItem[] = []
-
-
-  ngOnInit(): void {
-    this.clothingItemService.clothes$
+  clothes$: Observable<ClothingItem[]> = this.clothingItemService.clothes$
       .pipe(
-        takeUntil(this.destroySubject$),
         map(clothes => clothes.slice(0, 3))
       )
-      .subscribe(clothes => {
-        this.clothes = clothes
-      })
-  }
 
+      constructor() {}
 
-  ngOnDestroy(): void {
-    this.destroySubject$.next(null)
-    this.destroySubject$.complete()
-  }
 }
