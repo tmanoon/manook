271c7e89c255903d8a04b18ff85915df8399ace9
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-26 19:11:07 +0300

    Add functionality to clothing item list component

--- frontend/src/app/cmps/clothing-item-list/clothing-item-list.component.ts
+++ frontend/src/app/cmps/clothing-item-list/clothing-item-list.component.ts
@@ -1,12 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, inject } from '@angular/core';
 import { ClothingItem } from '../../models/clothingitem.model';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
+import { Observable, Subject, retry, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'clothing-item-list',
   templateUrl: './clothing-item-list.component.html',
   styleUrl: './clothing-item-list.component.scss'
 })
-export class ClothingItemListComponent {
+export class ClothingItemListComponent implements OnInit, OnDestroy {
+  
+  @Input() clothes!: ClothingItem[] | null
+  private userService = inject(UserService)
+  disconnectedUserClicked: boolean = false
+  destroySubject$ = new Subject()
+  user!: User | null
 
-  @Input() clothes!: ClothingItem[] 
+  ngOnInit(): void {
+    this.userService.loggedInUser$
+      .pipe(
+        takeUntil(this.destroySubject$),
+        retry(1)
+      )
+      .subscribe(user => this.user = user)
+  }
+
+  onAddClothingItem(item: ClothingItem): void {
+    this.disconnectedUserClicked = this.userService.addItemToOrder(item)
+    if(this.disconnectedUserClicked) this.showDisconnectedUserPopUp
+  }
+
+  showDisconnectedUserPopUp(): void {
+    this.disconnectedUserClicked = true
+    setTimeout(() => {
+      !this.disconnectedUserClicked
+    }, 1500)
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject$.next(null)
+    this.destroySubject$.complete()
+  }
 }
