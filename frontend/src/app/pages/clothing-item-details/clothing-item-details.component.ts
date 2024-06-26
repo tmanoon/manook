da98d58c26042bac1933e3070e3c8778d5ab23fc
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-27 00:08:20 +0300

    Add basic functionality to clothing item details component

--- frontend/src/app/pages/clothing-item-details/clothing-item-details.component.ts
+++ frontend/src/app/pages/clothing-item-details/clothing-item-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClothingItem } from '../../models/clothingitem.model';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'clothing-item-details',
   templateUrl: './clothing-item-details.component.html',
   styleUrl: './clothing-item-details.component.scss'
 })
-export class ClothingItemDetailsComponent {
 
+export class ClothingItemDetailsComponent {
+  private route = inject(ActivatedRoute)
+  private router = inject(Router)
+
+  clothingItem$: Observable<ClothingItem> = this.route.data.pipe(
+    map(data => {
+      console.log(data)
+      const clothingItem: ClothingItem = data['clothingItem']
+      return clothingItem
+    }))
+
+    onBack() {
+      this.router.navigateByUrl('/clothing-item')
+  }
 }
