9627f2993b707a8ac505fb16fca0bf50dbfd5cd2
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-30 12:17:00 +0300

    Move subscription to user to index component and get it as input from it

--- frontend/src/app/cmps/clothing-item-list/clothing-item-list.component.ts
+++ frontend/src/app/cmps/clothing-item-list/clothing-item-list.component.ts
@@ -9,49 +9,15 @@ import { Observable, Subject, retry, take, takeUntil, tap } from 'rxjs';
   templateUrl: './clothing-item-list.component.html',
   styleUrl: './clothing-item-list.component.scss'
 })
-export class ClothingItemListComponent implements OnInit, OnDestroy {
+export class ClothingItemListComponent  {
 
   @Output() addToCart = new EventEmitter<ClothingItem>()
   @Output() remove = new EventEmitter<string>()
   @Input() clothes!: ClothingItem[] | null
-  private userService = inject(UserService)
+  @Input() user!: User | null
   disconnectedUserClicked: boolean = false
   destroySubject$ = new Subject()
-  user!: User | null
   
-
-  ngOnInit(): void {
-    this.userService.loggedInUser$
-      .pipe(
-        takeUntil(this.destroySubject$),
-        retry(1)
-      )
-      .subscribe(user => this.user = user)
-  }
-
-  onAddClothingItem(item: ClothingItem): void {
-    this.userService.addItemToOrder(item)
-      .pipe(
-        tap(order => {
-          if(!order) this.disconnectedUserClicked = true 
-        }),
-        take(1)
-      ).subscribe()
-    if(this.disconnectedUserClicked) this.showDisconnectedUserPopUp()
-  }
-
-  showDisconnectedUserPopUp(): void {
-    this.disconnectedUserClicked = true
-    setTimeout(() => {
-      !this.disconnectedUserClicked
-    }, 1500)
-  }
-
-  ngOnDestroy(): void {
-    this.destroySubject$.next(null)
-    this.destroySubject$.complete()
-  }
-
   trackByFn(idx: number, item: ClothingItem) : string {
     return item._id
   }
