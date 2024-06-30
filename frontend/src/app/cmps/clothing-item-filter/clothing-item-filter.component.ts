a9bd1b6548253cffaf74cfc715a4e5358e77f812
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-30 19:06:46 +0300

    Add basic functionality to clothing item filter

--- frontend/src/app/cmps/clothing-item-filter/clothing-item-filter.component.ts
+++ frontend/src/app/cmps/clothing-item-filter/clothing-item-filter.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ClothingItemService } from '../../services/clothingitem.service';
+import { Observable, Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
+import { FilterBy } from '../../models/filterby.model';
 
 @Component({
   selector: 'clothing-item-filter',
   templateUrl: './clothing-item-filter.component.html',
   styleUrl: './clothing-item-filter.component.scss'
 })
-export class ClothingItemFilterComponent {
+export class ClothingItemFilterComponent implements OnInit, OnDestroy {
+  private clothingItemService = inject(ClothingItemService)
+  private filterSubject$ = new Subject()
+  private destroySubject$ = new Subject()
+  public filterBy!: FilterBy
 
+  ngOnInit(): void {
+
+    this.clothingItemService.filterBy$
+      .pipe(
+        takeUntil(this.destroySubject$)
+      )
+      .subscribe({
+        next: filterBy => this.filterBy = filterBy,
+        error: err => console.log('err', err)
+      })
+
+    this.filterSubject$
+      .pipe(
+        debounceTime(200),
+        takeUntil(this.destroySubject$)
+      )
+      .subscribe(() =>
+        this.clothingItemService.setFilter(this.filterBy)
+      )
+  }
+
+  onSetStyle(e: Event) {
+    const chosenStyle: string = (e.target as HTMLInputElement).value
+    const isChecked: boolean = (e.target as HTMLInputElement).checked
+    console.log(chosenStyle, isChecked)
+    if (isChecked) this.filterBy.style.push(chosenStyle)
+    else {
+      this.filterBy.style = this.filterBy.style.filter(style => style !== chosenStyle)
+    }
+    this.onSetFilter(chosenStyle)
+  }
+
+  onSetFilter(term: string) {
+    this.filterSubject$.next(term)
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject$.next(null)
+    this.destroySubject$.complete()
+  }
 }
