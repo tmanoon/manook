35a6c2fdc9a0dc677511feefd7b190a93025f2d4
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-30 19:59:48 +0300

    Add functionality to dynamic style input cmp

--- frontend/src/app/cmps/dynamic-style-input/dynamic-style-input.component.ts
+++ frontend/src/app/cmps/dynamic-style-input/dynamic-style-input.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FilterBy } from '../../models/filterby.model';
 
+export type selectedStyleChange = {
+  isChecked: boolean, 
+  chosenStyle: string
+}
 @Component({
   selector: 'dynamic-style-input',
   templateUrl: './dynamic-style-input.component.html',
   styleUrl: './dynamic-style-input.component.scss'
 })
+
 export class DynamicStyleInputComponent {
 
+  @Input() filterBy!: FilterBy
+  @Input() style!: string
+  @Output() select = new EventEmitter<selectedStyleChange>()
+
+  onSetStyle(e: Event) {
+    const ev = e.target as HTMLInputElement
+    const selection: selectedStyleChange = {
+      isChecked: ev.checked,
+      chosenStyle: ev.value
+    }
+    this.select.emit(selection)
+  }
 }
