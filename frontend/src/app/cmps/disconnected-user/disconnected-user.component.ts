4eb953649f5eb4ae7a94af5d838dddc4cb1de3aa
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-07-01 13:17:24 +0300

    Add functionality to disconnected user component

--- frontend/src/app/cmps/disconnected-user/disconnected-user.component.ts
+++ frontend/src/app/cmps/disconnected-user/disconnected-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'disconnected-user',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './disconnected-user.component.scss'
 })
 export class DisconnectedUserComponent {
+
+  @Output() close = new EventEmitter<boolean>()
+
+  onCloseMsg() : void {
+    this.close.emit(false)
+  }
 
 }
