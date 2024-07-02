9badc01f20593eb41e16b28571ea802711d889e8
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-07-02 17:38:25 +0300

    Add functionality to user wishlist component

--- frontend/src/app/pages/user-wishlist/user-wishlist.component.ts
+++ frontend/src/app/pages/user-wishlist/user-wishlist.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
+import { Observable, Subject, retry, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'user-wishlist',
   templateUrl: './user-wishlist.component.html',
   styleUrl: './user-wishlist.component.scss'
 })
-export class UserWishlistComponent {
+export class UserWishlistComponent implements OnInit, OnDestroy {
 
+  private userService = inject(UserService)
+  private destroySubject$ = new Subject()
+  user!: User
+
+  ngOnInit(): void {
+    this.userService.loggedInUser$
+      .pipe(
+        retry(1),
+        takeUntil(this.destroySubject$),
+        tap(user => {
+          this.user = user!
+        })
+      )
+      .subscribe({
+        error: err => console.log('err', err)
+      })
+  }
+
+  onRemoveItemFromWishlist(id: string) {
+    if (this.user!.wishlist.find(item => item._id === id)) return
+    this.userService.removeItemFromWishlist(id)
+      .pipe(
+        take(1)
+      )
+      .subscribe({
+        error: err => console.log('err', err)
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject$.next(null)
+    this.destroySubject$.complete()
+  }
 }
