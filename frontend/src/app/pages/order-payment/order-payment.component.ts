39012014d6efe1b94ac0e7586938ee61b0006021
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-07-04 16:44:44 +0300

    Move functionality to user order component

--- frontend/src/app/pages/order-payment/order-payment.component.ts
+++ frontend/src/app/pages/order-payment/order-payment.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Observable, Subject, retry, take, takeUntil, tap } from 'rxjs';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'order-payment',
@@ -6,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './order-payment.component.scss'
 })
 export class OrderPaymentComponent {
+
 
 }
--- frontend/src/app/pages/user-order/user-order.component.ts
+++ frontend/src/app/pages/user-order/user-order.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Subject, retry, take, takeUntil, tap } from 'rxjs';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'user-order',
@@ -7,4 +10,40 @@ import { Component } from '@angular/core';
 })
 export class UserOrderComponent {
 
+  private userService = inject(UserService)
+  loggedInUser!: User
+  destroySubject$ = new Subject()
+
+  ngOnInit(): void {
+    this.userService.loggedInUser$
+      .pipe(
+        takeUntil(this.destroySubject$),
+        retry(1),
+        tap(user => {
+          this.loggedInUser = user!
+        })
+      )
+      .subscribe({
+        error: err => console.log('err', err)
+      })
+  }
+
+  onRemoveItemFromWishlist(id: string) {
+    this.userService.removeItemFromWishlist(id)
+      .pipe(
+        take(1),
+        retry(1)
+      )
+      .subscribe({
+        error: err => console.log('err', err)
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject$.next(null)
+    this.destroySubject$.complete()
+  }
+
 }
+
+
