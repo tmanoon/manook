73919585dda5f69986eae44ee11820a37f520499
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-08-19 21:46:38 +0300

    Add functionality to order list component

--- frontend/src/app/cmps/order-list/order-list.component.ts
+++ frontend/src/app/cmps/order-list/order-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Order } from '../../models/order.model';
 
 @Component({
   selector: 'order-list',
   templateUrl: './order-list.component.html',
   styleUrl: './order-list.component.scss'
 })
-export class OrderListComponent {
+export class OrderListComponent implements OnInit {
 
+  @Input() orders!: Order[]
+
+  ngOnInit(): void {
+    this.orders = this.orders.slice(0, 3)
+  }
+
+  trackByFn(idx: number, item: Order) : string {
+    return item._id
+  }
 }
