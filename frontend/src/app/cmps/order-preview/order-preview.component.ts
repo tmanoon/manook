88b6feffe0817ea8c2d57ecbeec2116a5ada6e7d
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-08-22 16:43:53 +0300

    Create basic functionality to order preview component

--- frontend/src/app/cmps/order-preview/order-preview.component.ts
+++ frontend/src/app/cmps/order-preview/order-preview.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Order } from '../../models/order.model';
+import { ClothingItem } from '../../models/clothingitem.model';
 
 @Component({
   selector: 'order-preview',
   templateUrl: './order-preview.component.html',
   styleUrl: './order-preview.component.scss'
 })
-export class OrderPreviewComponent {
+export class OrderPreviewComponent implements OnInit {
   @Input() order!: Order
-  
+ 
+  ngOnInit(): void {
+    this.order.selectedItems.slice(0, 4)
+  }
+ 
+  trackByFn(idx: number, item: ClothingItem) {
+    return item._id
+  }
 }
